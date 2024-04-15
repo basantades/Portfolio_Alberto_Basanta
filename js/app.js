ce7b51706df103425ead101655d980e6ae55a7a7
basantades <basantades@gmail.com>
2024-04-15 12:59:14 +0200

    cambios dia 15

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 const rootcss = document.getElementById('rootcss');
 const iconoColor = document.getElementById('iconoColor');
+let punto1 = document.querySelector('.punto1');
+let punto2 = document.querySelector('.punto2');
+let punto3 = document.querySelector('.punto3');
+let punto4 = document.querySelector('.punto4');
 function changeColor() {
     if (rootcss.getAttribute('href') === 'css/root.css' || rootcss.getAttribute('href') === "css/root.css") {
         rootcss.setAttribute('href', 'css/root2.css');
+        punto1.classList.remove('actual');
+        punto2.classList.add('actual');
     } else if (rootcss.getAttribute('href') === 'css/root2.css') {
         rootcss.setAttribute('href', 'css/root3.css');
+        punto2.classList.remove('actual');
+        punto3.classList.add('actual');
     } else if (rootcss.getAttribute('href') === 'css/root3.css') {
         rootcss.setAttribute('href', 'css/root4.css');
+        punto3.classList.remove('actual');
+        punto4.classList.add('actual');
     }
     else {
         rootcss.setAttribute('href', 'css/root.css');
+        punto4.classList.remove('actual');
+        punto1.classList.add('actual');
     }
 }
 iconoColor.addEventListener('click', changeColor);
