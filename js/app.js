7a179a1969c758955a1029cd71ff5150131074e8
basantades <basantades@gmail.com>
2024-04-25 13:52:03 +0200

    cambios 25

--- js/app.js
+++ js/app.js
@@ -43,3 +43,45 @@ function changeTheme() {
 }
 iconoLight.addEventListener('click', changeTheme);
 iconoDark.addEventListener('click', changeTheme);
+
+
+
+
+
+
+
+
+
+
+
+// Maneja el evento mousemove en el contenedor de parallax
+document.querySelector('.parallax').addEventListener('mousemove', function(e) {
+    // Calcula la posición del ratón dentro del contenedor
+    var mouseX = e.clientX - this.offsetLeft;
+    var mouseY = e.clientY - this.offsetTop;
+    
+    // Calcula los desplazamientos para cada imagen
+    var offsetX = (mouseX - (this.offsetWidth / 2)) / this.offsetWidth * 20;
+    var offsetY = (mouseY - (this.offsetHeight / 2)) / this.offsetHeight * 40;
+    
+    // Aplica los desplazamientos a cada imagen con un efecto de transición suave
+    imgpara2.style.transform = 'translate(' + offsetX * 0.6 + 'px, ' + offsetY * 0.1 + 'px)';
+    imgpara3.style.transform = 'translate(' + offsetX * 0.4 + 'px, ' + offsetY * 0.2 + 'px)';
+    imgpara4.style.transform = 'translate(' + offsetX * 0.9 + 'px, ' + offsetY * 0.3 + 'px)';
+    imgpara5.style.transform = 'translate(' + offsetX * 0.6 + 'px, ' + offsetY * 0.1 + 'px)';
+    imgpara6.style.transform = 'translate(' + offsetX * 0.4 + 'px, ' + offsetY * 0.2 + 'px)';
+    imgpara7.style.transform = 'translate(' + offsetX * 0.3 + 'px, ' + offsetY * 0.3 + 'px)';
+    imgpara8.style.transform = 'translate(' + offsetX * 3 + 'px, ' + offsetY * 0.1 + 'px)';
+    imgpara9.style.transform = 'translate(' + offsetX * 1 + 'px, ' + offsetY * 0.2 + 'px)';
+    imgpara10.style.transform = 'translate(' + offsetX * 5 + 'px, ' + offsetY * 0.3 + 'px)';
+    imgpara11.style.transform = 'translate(' + offsetX * 7 + 'px, ' + offsetY * 0.1 + 'px)';
+    imgpara12.style.transform = 'translate(' + offsetX * 5 + 'px, ' + offsetY * 0.2 + 'px)';
+    imgpara13.style.transform = 'translate(' + offsetX * 2 + 'px, ' + offsetY * 0.3 + 'px)';
+    imgpara14.style.transform = 'translate(' + offsetX * 1 + 'px, ' + offsetY * 0.3 + 'px)';
+  
+  
+    // Y así sucesivamente para cada imagen, ajustando los valores de offsetX y offsetY según sea necesario
+  
+    // Modifica el margen izquierdo de la imagen 5
+    // imgpara5.style.marginLeft = offsetX * 2 + 'px';
+  }); 
