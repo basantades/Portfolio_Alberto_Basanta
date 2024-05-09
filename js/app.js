cc38db87b33eae3cbc547c169d680af9ead403cb
Alberto Basanta <dasantades@gmail.com>
2024-05-10 00:49:41 +0200

    cambios dia 10

--- js/app.js
+++ js/app.js
@@ -1,91 +1,87 @@
 // Nodos para cambio de root
-const rootcss = document.getElementById('rootcss');
-const iconoColor = document.getElementById('iconoColor');
-let punto1 = document.querySelector('.punto1');
-let punto2 = document.querySelector('.punto2');
-let punto3 = document.querySelector('.punto3');
-let punto4 = document.querySelector('.punto4');
-let textoHero = document.querySelector('.textHero');
+const rootcss = document.getElementById("rootcss");
+const iconoColor = document.getElementById("iconoColor");
+let punto1 = document.querySelector(".punto1");
+let punto2 = document.querySelector(".punto2");
+let punto3 = document.querySelector(".punto3");
+let punto4 = document.querySelector(".punto4");
+let textoHero = document.querySelector(".textHero");
 const contenidoTextoHero = textoHero.innerHTML;
-
 
 // Funcion para cambio de root
 function changeColor() {
-    if (rootcss.getAttribute('href') === 'css/root2.css') {
-        rootcss.setAttribute('href', 'css/root3.css');
-        punto2.classList.remove('actual');
-        punto3.classList.add('actual');
-    } else if (rootcss.getAttribute('href') === 'css/root3.css') {
-        rootcss.setAttribute('href', 'css/root4.css');
-        punto3.classList.remove('actual');
-        punto4.classList.add('actual');
-    } else if (rootcss.getAttribute('href') === 'css/root4.css') {
-        rootcss.setAttribute('href', 'css/root.css');
-        punto4.classList.remove('actual');
-        punto1.classList.add('actual');
-    }
-    else {
-        rootcss.setAttribute('href', 'css/root2.css');
-        punto1.classList.remove('actual');
-        punto2.classList.add('actual');
-    }
-    textoHero.innerHTML = '';
-    textoHero.innerHTML = contenidoTextoHero;
+  if (rootcss.getAttribute("href") === "css/root2.css") {
+    rootcss.setAttribute("href", "css/root3.css");
+    punto2.classList.remove("actual");
+    punto3.classList.add("actual");
+  } else if (rootcss.getAttribute("href") === "css/root3.css") {
+    rootcss.setAttribute("href", "css/root4.css");
+    punto3.classList.remove("actual");
+    punto4.classList.add("actual");
+  } else if (rootcss.getAttribute("href") === "css/root4.css") {
+    rootcss.setAttribute("href", "css/root.css");
+    punto4.classList.remove("actual");
+    punto1.classList.add("actual");
+  } else {
+    rootcss.setAttribute("href", "css/root2.css");
+    punto1.classList.remove("actual");
+    punto2.classList.add("actual");
+  }
+  textoHero.innerHTML = "";
+  textoHero.innerHTML = contenidoTextoHero;
 }
 
 // Event listener para cambio de root
-iconoColor.addEventListener('click', changeColor);
-
+iconoColor.addEventListener("click", changeColor);
 
 // Nodos para cambio claro-oscuro
-const iconoLight = document.getElementById('iconoLight');
-const iconoDark = document.getElementById('iconoDark');
-const html = document.querySelector('html');
+const iconoLight = document.getElementById("iconoLight");
+const iconoDark = document.getElementById("iconoDark");
+const html = document.querySelector("html");
 
 // funcion para cambio claro-oscuro
 function changeTheme() {
-    if (html.getAttribute('class') === 'dark') {
-        html.setAttribute('class', 'light');
-        iconoLight.classList.add('ocultar');
-        iconoDark.classList.remove('ocultar');
-    } else {
-        html.setAttribute('class', 'dark');
-        iconoDark.classList.add('ocultar');
-        iconoLight.classList.remove('ocultar');
-    }
-    textoHero.innerHTML = '';
-    textoHero.innerHTML = contenidoTextoHero;
+  if (html.getAttribute("class") === "dark") {
+    html.setAttribute("class", "light");
+    iconoLight.classList.add("ocultar");
+    iconoDark.classList.remove("ocultar");
+  } else {
+    html.setAttribute("class", "dark");
+    iconoDark.classList.add("ocultar");
+    iconoLight.classList.remove("ocultar");
+  }
+  textoHero.innerHTML = "";
+  textoHero.innerHTML = contenidoTextoHero;
 }
 
 // Event listener para cambio claro-oscuro
-iconoLight.addEventListener('click', changeTheme);
-iconoDark.addEventListener('click', changeTheme);
-
+iconoLight.addEventListener("click", changeTheme);
+iconoDark.addEventListener("click", changeTheme);
 
 // Nodos para cambio menu movil
-let iconoMenu = document.getElementById('iconoMenu');
-let iconoMenuClose = document.getElementById('iconoMenuClose');
-let desplegableMenu = document.getElementById('desplegableMenu');
+let iconoMenu = document.getElementById("iconoMenu");
+let iconoMenuClose = document.getElementById("iconoMenuClose");
+let desplegableMenu = document.getElementById("desplegableMenu");
 
 // Funcion para abrir menu
 function abrirMenu() {
-    desplegableMenu.classList.remove('ocultar');
-    desplegableMenu.classList.remove('animacionEsconderMenu');
-    desplegableMenu.classList.add('animacionAparecerMenu');
-    iconoMenu.classList.add('ocultar');
-    iconoMenuClose.classList.remove('ocultar');
+  desplegableMenu.classList.remove("ocultar");
+  desplegableMenu.classList.remove("animacionEsconderMenu");
+  desplegableMenu.classList.add("animacionAparecerMenu");
+  iconoMenu.classList.add("ocultar");
+  iconoMenuClose.classList.remove("ocultar");
 }
 function cerrarMenu() {
-    desplegableMenu.classList.remove('animacionAparecerMenu');
-    desplegableMenu.classList.add('animacionEsconderMenu');
-    iconoMenu.classList.remove('ocultar');
-    iconoMenuClose.classList.add('ocultar');
+  desplegableMenu.classList.remove("animacionAparecerMenu");
+  desplegableMenu.classList.add("animacionEsconderMenu");
+  iconoMenu.classList.remove("ocultar");
+  iconoMenuClose.classList.add("ocultar");
 }
 
 // Event listener para abrir y cerrar menu
-iconoMenu.addEventListener('click', abrirMenu);
-iconoMenuClose.addEventListener('click', cerrarMenu);
+iconoMenu.addEventListener("click", abrirMenu);
+iconoMenuClose.addEventListener("click", cerrarMenu);
 
-window.addEventListener('scroll', () => {
-    cerrarMenu();
+window.addEventListener("scroll", () => {
+  cerrarMenu();
 });
--- js/menu-desk.js
+++ js/menu-desk.js
@@ -1,38 +1,40 @@
 // Espera a que el contenido del DOM esté cargado
-document.addEventListener('DOMContentLoaded', () => {
-    // Selecciona todos los enlaces del menú y las secciones
-    const menuLinks = document.querySelectorAll('nav .menu a');
-    const sections = document.querySelectorAll('section');
-  
-    // Función para remover la clase 'active' de todos los enlaces del menú
-    function clearMenu() {
-      menuLinks.forEach(link => {
-        link.classList.remove('active');
-      });
-    }
-  
-    // Función para activar el enlace del menú basado en el desplazamiento actual
-    function activateMenuLink() {
-      let currentActiveIndex = -1;
-      sections.forEach((section, index) => {
-          const sectionTop = section.offsetTop;
-          const sectionHeight = section.offsetHeight;
-          const sectionBottom = sectionTop + sectionHeight;
-          const viewportHeight = window.innerHeight;
+document.addEventListener("DOMContentLoaded", () => {
+  // Selecciona todos los enlaces del menú y las secciones
+  const menuLinks = document.querySelectorAll("nav .menu a");
+  const sections = document.querySelectorAll("section");
 
-   // Verifica si el usuario ve más del 50% de la sección actual
-   if (window.scrollY >= sectionTop - viewportHeight / 2 && window.scrollY < sectionBottom) {
-    currentActiveIndex = index;
-}
-});
-      clearMenu();
-      // Si hay una sección activa, añade la clase 'active'
-      if (currentActiveIndex !== -1) {
-        menuLinks[currentActiveIndex].classList.add('active');
+  // Función para remover la clase 'active' de todos los enlaces del menú
+  function clearMenu() {
+    menuLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+  }
+
+  // Función para activar el enlace del menú basado en el desplazamiento actual
+  function activateMenuLink() {
+    let currentActiveIndex = -1;
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionBottom = sectionTop + sectionHeight;
+      const viewportHeight = window.innerHeight;
+
+      // Verifica si el usuario ve más del 50% de la sección actual
+      if (
+        window.scrollY >= sectionTop - viewportHeight / 2 &&
+        window.scrollY < sectionBottom
+      ) {
+        currentActiveIndex = index;
+      }
+    });
+    clearMenu();
+    // Si hay una sección activa, añade la clase 'active'
+    if (currentActiveIndex !== -1) {
+      menuLinks[currentActiveIndex].classList.add("active");
     }
-}
-  
-    // Añade un 'event listener' para el evento de desplazamiento
-    window.addEventListener('scroll', activateMenuLink);
-  });
-  
+  }
+
+  // Añade un 'event listener' para el evento de desplazamiento
+  window.addEventListener("scroll", activateMenuLink);
+});
